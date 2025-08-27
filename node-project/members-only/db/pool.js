33a313e2c1afbbe0a1742902d6f2dce45aeb4438
Yufan Zhang <zhang.yufan029@gmail.com>
2025-08-27 22:28:24 +1000

    Add missing typo - fail to connect to the database

--- node-project/members-only/db/pool.js
+++ node-project/members-only/db/pool.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    host: process.env.DATABSE_HOST,
+    host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     database: process.env.DATABASE_NAME,
     password: process.env.DATABASE_PASSWORD,
